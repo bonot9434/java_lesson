0f0e2a7fa6986198aa13385958d1182e78b096a9
bonot9434 <bonot9434@gmail.com>
2021-12-12 19:38:14 +0900

    Chapter04

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+    public static void main(String[] args){
+        final int LUCKY_NUMBER = 7;
+        String str = "Taro";
+        System.out.println(str);
+        str = "好きな言葉";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+}
